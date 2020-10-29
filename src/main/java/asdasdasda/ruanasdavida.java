a92066c2f1e883594b7796bdd06e23ead0b26ba2
MatEvangelista <mathews.pee@gmail.com>
2020-10-29 17:46:43 -0300

    Criando ruanasdavida.java

---
+++ src/main/java/asdasdasda/ruanasdavida.java
@@ -0,0 +1,4 @@
+package asdasdasda;
+
+public class ruanasdavida {
+}
--- src/main/java/excecoes/programaPrincipal/ProgramaMain.java
+++ src/main/java/excecoes/programaPrincipal/ProgramaMain.java
@@ -20,23 +20,27 @@ public class ProgramaMain {
         try {
             System.out.print("Room number: ");
             int number = sc.nextInt();
+
             System.out.print("Check-in date (dd/MM/yyyy): ");
             Date checkIn = sdf.parse(sc.next());
+
             System.out.print("Check-out date (dd/MM/yyyy): ");
             Date checkOut = sdf.parse(sc.next());
 
             Reservation reservation = new Reservation(number, checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
-
             System.out.println();
             System.out.println("Enter data to update the reservation:");
+
             System.out.print("Check-in date (dd/MM/yyyy): ");
             checkIn = sdf.parse(sc.next());
+
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
             reservation.updateDates(checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
+
         } catch (ParseException e) {
             System.out.println("Invalid date format");
         } catch (DomainException e) {
